4e408aebd72a2aba653d3172e03d14b2a23553ed
Andrey <andreyrazzvoz@gmail.com>
2024-06-15 21:19:56 +0300

    Update keys.py

--- backend/keys.py
+++ backend/keys.py
@@ -7,7 +7,9 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 
-app = FastAPI()
+app = FastAPI(
+    title="title"
+)
 
 class Keys(BaseModel):
     privatekey: str
