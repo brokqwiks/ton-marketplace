a059535501e54e0099f72167addafb8ecf51e909
brokqwiks <brokqwiks@mail.ru>
2024-06-23 13:31:21 +0300

    'manifest'

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header/Header"
 import Home from "./pages/Home/Home"
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 function App() {
   return (
     <>
+    <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/brokqwiks/ton-marketplace/main/manifest.json">
       <Header />
       <Home />
+    </TonConnectUIProvider>
     </>
   )
 }
